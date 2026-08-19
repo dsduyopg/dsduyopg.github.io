35669c2800acb08256bb0f009091b2f7a5e3a90e
dsduyopg <629017960@qq.com>
2026-08-20 00:26:23 +0800

    auto-sync: 2026-08-20 00:26:22

--- 启动预览.bat
+++ 启动预览.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d D:\my-blog
-start "" "C:\ProgramData\chocolatey\lib\hugo	ools\hugo.exe" server --port 1313 --bind 127.0.0.1
+start "" "C:\ProgramData\chocolatey\lib\hugo\tools\hugo.exe" server --port 1313 --bind 127.0.0.1
